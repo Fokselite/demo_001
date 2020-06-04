4d845b9eb21a98808b04b454592dfd16fdbb5a8d
Fokselite <Fokselite@users.noreply.github.com>
2020-06-04 13:10:34 +0500

    Updates

---
+++ precache-manifest.883a58972d4c8e9b93cdf2f54c1ce8ea.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f131286266cfa9becbe4ae8a37ef108",
+    "url": "/demo_001/index.html"
+  },
+  {
+    "revision": "2d9c71c28a3599a5a8d1",
+    "url": "/demo_001/static/css/3.95df5104.chunk.css"
+  },
+  {
+    "revision": "6e55d87a477079174534",
+    "url": "/demo_001/static/css/4.f1339459.chunk.css"
+  },
+  {
+    "revision": "96cea37b9c4f53f6e6a0",
+    "url": "/demo_001/static/css/main.1d554d02.chunk.css"
+  },
+  {
+    "revision": "8807000f6f2baddceb03",
+    "url": "/demo_001/static/js/2.3ba03a99.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/demo_001/static/js/2.3ba03a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9c71c28a3599a5a8d1",
+    "url": "/demo_001/static/js/3.57859285.chunk.js"
+  },
+  {
+    "revision": "6e55d87a477079174534",
+    "url": "/demo_001/static/js/4.bedc649a.chunk.js"
+  },
+  {
+    "revision": "20a59350a66477956fe7",
+    "url": "/demo_001/static/js/5.db227401.chunk.js"
+  },
+  {
+    "revision": "150e2cf9ad0a3e3f42e3",
+    "url": "/demo_001/static/js/6.55076df8.chunk.js"
+  },
+  {
+    "revision": "5f63ecd42c4bc6ac4bc3",
+    "url": "/demo_001/static/js/7.71dab25c.chunk.js"
+  },
+  {
+    "revision": "96cea37b9c4f53f6e6a0",
+    "url": "/demo_001/static/js/main.62567b26.chunk.js"
+  },
+  {
+    "revision": "ad3dac5773ccf5f75435",
+    "url": "/demo_001/static/js/runtime-main.9f04ab46.js"
+  },
+  {
+    "revision": "a0f000d51c36fe176b21329e2d96065d",
+    "url": "/demo_001/static/media/content-header-team.a0f000d5.jpg"
+  },
+  {
+    "revision": "54a2b8678235f4aedbdf0801a25a50bf",
+    "url": "/demo_001/static/media/content-profile-ico.54a2b867.png"
+  },
+  {
+    "revision": "57615b8c0092a66c1d4058b1692955cc",
+    "url": "/demo_001/static/media/loading-4.57615b8c.gif"
+  },
+  {
+    "revision": "34bc31454da41f242abcd9d836e3934f",
+    "url": "/demo_001/static/media/logo-js-2.34bc3145.png"
+  },
+  {
+    "revision": "6218f783efedb04158dbdf7ca364f7c6",
+    "url": "/demo_001/static/media/no_djob.6218f783.png"
+  }
+]);
